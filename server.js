bae528691ff685cef41586eac53986a8057dae02
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-01-13 15:43:50 +0900

    mongoose, req and res test done

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/login', (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+  const validUser = {
+    email: "bkim123@gmail.com", 
+    pass: "1234"
+  }
+
+  if (email == validUser.email && password == validUser.pass){
+    res.send({
+      status: "valid"
+    });
+  } else {
+    res.send(404, {
+      status: "Not Valid"
+    })
+  }
 })
 
 app.listen(port, () => {
